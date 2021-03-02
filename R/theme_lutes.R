be557eaa12fa5a9aac1ad13db8b76edebef33086
npaterno <nicholas.paterno@gmail.com>
2021-03-02 12:24:59 -0800

    got theme_lutes to work! still just a draft

--- R/theme_lutes.R
+++ R/theme_lutes.R
@@ -1,37 +1,39 @@
-#' @title PLU Theme
-#' @description Theme for ggplot2 based on PLU colors
+#' {ggplot} theme.
+#'
 #' A ggplot theme based on PLU colors.
 #'
-#' @inheritParams ggplot2::theme_bw
+#' @author Nick Paterno
+#' @import ggplot2
+#' @export
+#' 
 
 
-theme_lutes <- function(base_size = 16, base_family = ""){
-  return(theme_bw()+
-    theme(
-    panel.background = element_rect(color = "#000"),
+
+theme_lutes <- function(){
+  theme(
+    panel.background = element_rect(color = "#808080",
+                                    fill = "#808080"),
     panel.grid.major.y = element_line(color = "#ffcc00",
-                                    size = 1.25),
+                                      size = 1.25),
     panel.grid.minor.y = element_line(color = "#cc9933"),
     panel.grid.major.x = element_blank(),
     panel.grid.minor.x = element_blank(),
     plot.background = element_rect(fill = "#808080"),
-    plot.title = element_text(color = "#fff",
-                         family = "mono",
-                         size = 16,
-                         face = "italic"),
-    plot.caption = element_text(color = "#fff",
+    plot.title = element_text(color = "#ffffff",
+                              family = "mono",
+                              size = 16,
+                              face = "italic"),
+    plot.caption = element_text(color = "#ffffff",
                                 family = "mono",
                                 size = 10,
                                 face = "italic"),
-    axis.text = element_text(color = "#fff",
+    axis.text = element_text(color = "#ffffff",
                              family = "mono",
                              size = 12),
     axis.ticks = element_blank(),
     axis.line.y = element_blank(),
-    axis.line.x = element_line(color = "#ffcc00",
-                               size = 1.5),
-    axis.title = element_text(color = "#fff",
+    axis.title = element_text(color = "#ffffff",
                               family = "mono",
                               size = 14)
-  ))
+  )
 }
